a75e2779c7f3817a605e3d37779c4a36f300b31d
parthraj12 <parthraj1994@gmail.com>
2022-05-28 16:54:14 +0530

    Initial commit

---
+++ savedata.php
@@ -0,0 +1,15 @@
+<?php
+        $stu_name=$_POST['sname'];
+        $stu_address=$_POST['saddress'];
+        $stu_class=$_POST['class'];
+        $stu_phone=$_POST['sphone'];
+
+        include'config.php';
+
+     $sql="INSERT INTO student(sname,saddress,sclass,sphone) VALUES ('{$stu_name}','{$stu_address}','{$stu_class}','{$stu_phone}')";
+
+     $result=mysqli_query($conn,$sql) or die("Please check query.");
+
+     header("Location:http://localhost/php_CRUD/index.php");
+     mysqli_close($conn);
+     ?>
